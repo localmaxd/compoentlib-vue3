e85c4b43b58c1f05e3f3f94caced8852706c2a12
localmaxd <1483573670@qq.com>
2022-04-11 13:32:46 +0800

    init the ui-2

--- docs/.vuepress/compoents/Carousel/data/carouselData.js
+++ docs/.vuepress/compoents/Carousel/data/carouselData.js
@@ -1,14 +1,14 @@
 export default [
   {
-    ImgUrl: "/img/1.jpeg",
+    ImgUrl: "https://img0.baidu.com/it/u=4152745790,1630191381&fm=253&fmt=auto&app=138&f=JPEG?w=282&h=500",
   },
   {
-    ImgUrl: "/img/2.jpeg",
+    ImgUrl: "https://img2.baidu.com/it/u=3896573322,1245897602&fm=253&fmt=auto&app=138&f=JPEG?w=889&h=500",
   },
   {
-    ImgUrl: "/img/3.jpeg",
+    ImgUrl: "https://img0.baidu.com/it/u=3879724579,2384271901&fm=253&fmt=auto&app=120&f=JPEG?w=1200&h=675",
   },
   {
-    ImgUrl:"/img/4.jpeg",
+    ImgUrl:"/https://img0.baidu.com/it/u=985192759,2265250910&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=333",
   }
 ];
